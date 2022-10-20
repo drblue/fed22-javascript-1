59f3290ec529d3a4020522e42657a29d2f824d39
Johan Nordström <johan@digitalvillage.se>
2022-10-20 09:55:34 +0200

    Numbers example.

---
+++ 02-numbers/script.js
@@ -0,0 +1,60 @@
+/*
+let width = 80;		// number
+let height = 40;	// number
+
+console.log(width + height);
+
+let circumference = width * 2 + height * 2;
+let area = width * height;
+
+console.log("Circumference of square is:", circumference);
+console.log("Area of square is:", area);
+*/
+
+/*
+let radius = 20;
+const pi = 3.14159;
+let circ = 2 * pi * radius;
+let a = pi * radius ** 2;
+
+console.log("Circumference of circle is:", circ);
+console.log("Area of circle is:", a);
+
+console.log("10 divided by 3 is:", 10 / 3);
+
+console.log("Modulus of 10 divided by 3:", 10 % 3);
+console.log("Modulus of 11 divided by 3:", 11 % 3);
+console.log("Modulus of 12 divided by 3:", 12 % 3);
+*/
+
+/*
+let points = 10;
+// points++;
+console.log("Points:", ++points);
+console.log("Points after increase:", points);
+*/
+
+// math operators
+let score = 50;
+score = score + 5;   // 50 + 5, new score is 55
+score = score - 15;  // 55 - 15, new score is 40
+score = score * 2;   // 40 * 2, new score is 80
+score = score / 4;   // 80 / 4, new score is 20
+
+// shorthand math operators
+score = 50;
+score += 5;    // score = score + 5
+score -= 15;   // score = score - 15
+score *= 2;    // score = score * 2
+score /= 4;    // score = score / 4
+
+// NaN - Not a Number
+console.log("10 divided by 2:", 10 / 2);
+console.log(5 + " points");
+console.log("Points: " + 2);
+
+console.log("Posts" - 5); // 😳
+
+console.log("Batman" * 5); // ??
+
+console.log("Captain America" / 2);
