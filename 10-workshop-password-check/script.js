8a3d0410ce2c9d85f2f67db58f36dee67f87b33a
Johan Nordström <johan@digitalvillage.se>
2022-10-25 16:11:58 +0200

    Solution to workshop "Password Checker".

--- 10-workshop-password-check/script.js
+++ 10-workshop-password-check/script.js
@@ -9,10 +9,10 @@
  */
 
 let password;
-password = "password"; // inte giltigt
+// password = "password"; // inte giltigt
 // password = "pa$sword"; // giltigt
 // password = "p@ssw%rd"; // giltigt
-// password = "pa$$word"; // giltigt
+password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
 // password = "such-password-much-secure-very-long"; // giltigt
@@ -20,3 +20,37 @@ password = "password"; // inte giltigt
 const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
+
+// set status variable to initial value false
+let hasSpecialChar = false
+
+// check for any special chars in password
+// for each specialChar in specialChars
+//   does password include specialChar?
+//   if true, set hasSpecialChar to true
+for (let i = 0; i < specialChars.length; i++) {
+	const specialChar = specialChars[i]
+	if (password.includes(specialChar)) {
+		hasSpecialChar = true
+	}
+}
+
+console.log(`🕵🏻 Checking password "${password}"`);
+
+// Har lösenordet minst 16 tecken?
+if (password.length >= 16) {
+	// Yes!
+	console.log("- ✅ Great! That's a long password!");
+
+// Har lösenordet minst 12 tecken OCH minst 1 bindestreck?
+} else if (password.length >= 12 && password.includes('-')) {
+	console.log("- ✅ Great! That's a pretty good password!");
+
+// Har lösenordet minst 8 tecken OCH minst 1 specialtecken?
+} else if (password.length >= 8 && hasSpecialChar) {
+	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
+
+} else {
+	// Inga andra krav uppfylldes
+	console.log("- 🚨 Insecure password, my grandma can crack it!");
+}
