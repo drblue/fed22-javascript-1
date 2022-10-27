ced7ec1a380b17a9a5d8bc2c08e49b59ca0e660b
Johan Nordström <johan@digitalvillage.se>
2022-10-27 10:58:13 +0200

    Solution for Password Check workshop.

--- 10-workshop-password-check/script.js
+++ 10-workshop-password-check/script.js
@@ -12,11 +12,43 @@ let password;
 // password = "password"; // inte giltigt
 // password = "pa$sword"; // giltigt
 // password = "p@ssw%rd"; // giltigt
-password = "pa$$word"; // giltigt
+// password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
-// password = "such-password-much-secure-very-long"; // giltigt
+password = "such-password-much-secure-very-long"; // giltigt
+
+console.log(`🕵🏻 Checking password '${password}'`);
 
 const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
+
+let hasSpecialChar = false
+
+// iterate (loop) over each item in specialChars
+for (let i = 0;  // only executed once, at the beginning of the loop
+	i < specialChars.length;  // should we continue another round?
+	i++  // AFTER each round, increase i by 1
+) {
+	const specialChar = specialChars[i]
+	console.log(`At index ${i}, does password contain '${specialChar}'?`, password.includes(specialChar));
+
+	if (password.includes(specialChar)) {
+		hasSpecialChar = true
+	}
+}
+
+console.log("Loop is done, found:", hasSpecialChar);
+
+if (password.length >= 16) {
+	console.log("- ✅ Great! That's a long password!");
+
+} else if (password.length >= 12 && password.includes('-')) {
+	console.log("- ✅ Great! That's a pretty good password!");
+
+} else if (password.length >= 8 && hasSpecialChar) {
+	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
+
+} else {
+	console.log("- 🚨 Insecure password, my grandma can crack it!");
+}
