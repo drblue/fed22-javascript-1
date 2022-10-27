47434f438cfd6fee31f27951484fe393449b6e81
Johan Nordström <johan@digitalvillage.se>
2022-10-27 09:23:48 +0200

    Reset of Password Check workshop.

--- 10-workshop-password-check/script.js
+++ 10-workshop-password-check/script.js
@@ -20,37 +20,3 @@ password = "pa$$word"; // giltigt
 const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
-
-// set status variable to initial value false
-let hasSpecialChar = false
-
-// check for any special chars in password
-// for each specialChar in specialChars
-//   does password include specialChar?
-//   if true, set hasSpecialChar to true
-for (let i = 0; i < specialChars.length; i++) {
-	const specialChar = specialChars[i]
-	if (password.includes(specialChar)) {
-		hasSpecialChar = true
-	}
-}
-
-console.log(`🕵🏻 Checking password "${password}"`);
-
-// Har lösenordet minst 16 tecken?
-if (password.length >= 16) {
-	// Yes!
-	console.log("- ✅ Great! That's a long password!");
-
-// Har lösenordet minst 12 tecken OCH minst 1 bindestreck?
-} else if (password.length >= 12 && password.includes('-')) {
-	console.log("- ✅ Great! That's a pretty good password!");
-
-// Har lösenordet minst 8 tecken OCH minst 1 specialtecken?
-} else if (password.length >= 8 && hasSpecialChar) {
-	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
-
-} else {
-	// Inga andra krav uppfylldes
-	console.log("- 🚨 Insecure password, my grandma can crack it!");
-}
