77de890a41f254ef3f9741311ab241f18b4b472e
Johan Nordström <johan@digitalvillage.se>
2022-11-04 11:17:51 +0100

    Traversing the DOM.

--- 19-dom/script.js
+++ 19-dom/script.js
@@ -2,6 +2,7 @@
  * DOM - Document Object Model
  */
 
+/*
 const firstLink = document.querySelector('#link-1');
 
 // Get the element's parent
@@ -31,3 +32,20 @@ console.log(firstErrorParagraph.innerText);
 // We can also change the content of an element
 firstParagraph.innerText = "Look ma, my first paragraph!";
 firstParagraph.innerText += " Oh I forgot this!";
+*/
+
+const alertEl = document.querySelector('.alert');
+console.log('alertEl:', alertEl);
+
+console.log('alertEl innerHTML:', alertEl.innerHTML);
+console.log('alertEl innerText:', alertEl.innerText);
+console.log('alertEl textContent:', alertEl.textContent);
+
+const linkListEl = document.querySelector('ul');
+console.log('linkListEl:', linkListEl.children);  // HTMLCollection, does not have `.forEach()`-method
+
+// Convert HTMLCollection to Array
+const liEls = Array.from(linkListEl.children);  // now we can use `.forEach()`
+
+// OR, we can use querySelector and querySelectorAll on the linkListEl
+const liEls2 = linkListEl.querySelectorAll('li');
