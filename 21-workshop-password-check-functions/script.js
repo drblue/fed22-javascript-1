d19b5675e43664cd1ef12d84ac3f1061400ca080
Johan Nordström <johan@digitalvillage.se>
2022-11-10 09:27:52 +0100

    Password check with functions - step 1.

--- 21-workshop-password-check-functions/script.js
+++ 21-workshop-password-check-functions/script.js
@@ -31,12 +31,47 @@ const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
 
-let password;
+let password = prompt("Please enter password to check:");
 // password = "password"; // inte giltigt
 // password = "p@$swo"; // giltigt
 // password = "pa$sword"; // giltigt
-password = "p@ssw%rd"; // giltigt
+// password = "p@ssw%rd"; // giltigt
 // password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
 // password = "such-password-much-secure-very-long"; // giltigt
+
+console.log(`🕵🏻 Checking password '${password}'`);
+
+let specialCharCount = 0
+
+// iterate (loop) over each item in specialChars
+for (let i = 0;  // only executed once, at the beginning of the loop
+	i < specialChars.length;  // should we continue another round?
+	i++  // AFTER each round, increase i by 1
+) {
+	const specialChar = specialChars[i]
+	console.log(`At index ${i}, does password contain '${specialChar}'?`, password.includes(specialChar));
+
+	if (password.includes(specialChar)) {
+		specialCharCount++
+	}
+}
+
+console.log("Loop is done, found:", specialCharCount);
+
+if (password.length >= 16) {
+	console.log("- ✅ Great! That's a long password!");
+
+} else if (password.length >= 12 && password.includes('-')) {
+	console.log("- ✅ Great! That's a pretty good password!");
+
+} else if (password.length >= 8 && specialCharCount) {
+	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
+
+} else if (password.length >= 6 && specialCharCount >= 2) {
+	console.log("- ✅ Great! Such password, much secure, VERY hard to crack!");
+
+} else {
+	console.log("- 🚨 Insecure password, my grandma can crack it!");
+}
