6f0cd5215904466adea7caf468b84ae2add79491
Johan Nordström <johan@digitalvillage.se>
2022-11-10 11:09:52 +0100

    Create a new listitem when button is clicked.

--- 22-dom-102-events/script.js
+++ 22-dom-102-events/script.js
@@ -3,3 +3,16 @@
  *
  */
 
+// get a reference to the button
+const btnAdd = document.querySelector('#add');
+
+// WHEN someone CLICKS on the BUTTON
+btnAdd.addEventListener('click', () => {
+	// DO this
+	console.log("oh, nice");
+
+	// GET reference to ALL `li` elements and their LENGTH
+	const liCount = document.querySelectorAll('li').length;
+
+	document.querySelector('ul').innerHTML += `<li>list item ${liCount + 1}</li>`;
+});
