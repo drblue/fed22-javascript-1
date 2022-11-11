796c5795621ebe1dc9cbd81c36fc56e36834aaae
Johan Nordström <johan@digitalvillage.se>
2022-11-11 15:11:21 +0100

    Tick tock.

--- 25-clock/script.js
+++ 25-clock/script.js
@@ -3,3 +3,15 @@
  *
  */
 
+// get reference to element with id `clock`
+const clockEl = document.querySelector('#clock');
+
+// we could stop the time by calling `clearInterval(clockId)`
+// start interval-timer with 1000 ms interval
+const clockId = setInterval( () => {
+	// get current date and time
+	const now = new Date();
+
+	// output current time to `#clock`-element
+	clockEl.innerText = now.toLocaleTimeString();
+}, 1000);
