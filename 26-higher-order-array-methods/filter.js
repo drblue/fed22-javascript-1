47b059a5e58e4a008608cee3fd95f18231dd9190
Johan Nordström <johan@digitalvillage.se>
2022-11-15 14:09:48 +0100

    Array filter (non-destructive).

--- 26-higher-order-array-methods/filter.js
+++ 26-higher-order-array-methods/filter.js
@@ -14,7 +14,7 @@
 // numbers
 const numbers = [ 47, 5, 21, 50, 24, 18, 47 ];
 
-console.log("Original numbers:", numbers);
+// console.log("Original numbers:", numbers);
 
 /*
 // push all numbers >= 25 in a new array
@@ -27,7 +27,10 @@ numbers.forEach(num => {
 */
 
 // filter out all numbers >= 25
+/*
 const largeNumbers = numbers.filter( num => {
+	return (num >= 25);
+
 	if (num >= 25) {
 		return true; // YES, this number should be in the new array
 	} else {
@@ -35,4 +38,39 @@ const largeNumbers = numbers.filter( num => {
 	}
 } );
 
+const largeNumbers = numbers.filter(num => num >= 25);
 console.log("Large numbers:", largeNumbers);
+*/
+
+/*
+const names = ["Johan", "Kajsa", "Bo", "Li"];
+const longishNames = names.filter(name => name.length > 2);
+console.log("Longish names:", longishNames);
+*/
+
+const students = [
+	{
+		name: "Johan",
+		points: 1337,
+	},
+	{
+		name: "Saman",
+		points: 3,
+	},
+	{
+		name: "Alicia",
+		points: 42,
+	},
+	{
+		name: "Elliot",
+		points: 88,
+	},
+	{
+		name: "Maja",
+		points: 35,
+	},
+];
+
+// get all students which are godkända (>= 40)
+const passed = students.filter(student => student.points >= 40);
+console.log("Students who passed the exam:", passed);
