2748e19733dfc281a11dd48bea5537e87fc841f5
Johan Nordström <johan@digitalvillage.se>
2022-11-22 09:58:38 +0100

    Workshop-solution: Guess the Number (DOM Edition) - Step 2.

--- 28-workshop-guess-the-number-dom-edition/app.js
+++ 28-workshop-guess-the-number-dom-edition/app.js
@@ -30,6 +30,7 @@
 const cheatEl = document.querySelector('#cheat');
 const formGuessEl = document.querySelector('#formGuess');
 const inputGuessEl = document.querySelector('#inputGuess');
+const guessesEl = document.querySelector('#guesses');
 const turnoutEl = document.querySelector('#turnout');
 
 // Get a random number between 1-10
@@ -56,6 +57,12 @@ formGuessEl.addEventListener('submit', e => {
 
 	// Get guessed number from input-field (and convert it to a Number)
 	const guessedNumber = Number(inputGuessEl.value);
+
+	// Increase number of guesses made
+	guesses++;
+
+	// Update DOM with guesses made
+	guessesEl.innerText = `${guesses} guesses`;
 
 	// Check if guess was correct
 	if (guessedNumber === correctNumber) {
