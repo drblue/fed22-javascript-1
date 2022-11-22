3baf862df219e410d31b42d2927cd269b50c09ea
Johan Nordström <johan@digitalvillage.se>
2022-11-22 15:34:52 +0100

    Attach listener to request, check that the request is done and that status is 200 OK. Then parse data from JSON and output the data to DOM.

--- 29-xmlhttprequest/script.js
+++ 29-xmlhttprequest/script.js
@@ -6,6 +6,34 @@
 // Create a new XML Http Request
 const request = new XMLHttpRequest();
 
+// Attach an event-listener to the request
+request.addEventListener('readystatechange', () => {
+	// Is request done?
+	if (request.readyState === 4) {    // 4 = DONE
+
+		// Was the request successful?
+		if (request.status === 200) {
+			// 200 OK
+
+			// take the STRING in responseText and PARSE it into JavaScript object
+			const data = JSON.parse(request.responseText);
+
+			console.log("Got me sum data:");
+			console.table(data);
+
+			// Loop over all users
+			data.forEach(user => {
+				// Append a listitem for each user
+				document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
+			});
+
+		} else {
+			// Something went wrong :'(
+			console.log("ERROR ERROR DANGER WILL ROBINSON!");
+		}
+	}
+});
+
 // Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
 request.open('GET', 'https://jsonplaceholder.typicode.com/users');
 
