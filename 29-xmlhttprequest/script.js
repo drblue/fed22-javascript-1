835cc0c40440af3a25365576a68b48f6e7c418e3
Johan Nordström <johan@digitalvillage.se>
2022-11-24 09:37:04 +0100

    Wrapped request in a function called `getUsers()`.

--- 29-xmlhttprequest/script.js
+++ 29-xmlhttprequest/script.js
@@ -3,42 +3,48 @@
  *
  */
 
-// Create a new XML Http Request
-const request = new XMLHttpRequest();
+const getUsers = () => {
+	// Create a new XML Http Request
+	const request = new XMLHttpRequest();
 
-// Attach an event-listener to the request
-request.addEventListener('readystatechange', () => {
-	// Is request done?
-	if (request.readyState === 4) {    // 4 = DONE
+	// Attach an event-listener to the request
+	request.addEventListener('readystatechange', () => {
+		// Is request done?
+		if (request.readyState === 4) {    // 4 = DONE
 
-		// Was the request successful?
-		if (request.status === 200) {
-			// 200 OK
+			// Was the request successful?
+			if (request.status === 200) {
+				// 200 OK
 
-			// take the STRING in responseText and PARSE it into JavaScript object
-			const data = JSON.parse(request.responseText);
+				// take the STRING in responseText and PARSE it into JavaScript object
+				const data = JSON.parse(request.responseText);
 
-			console.log("Got me sum data:");
-			console.table(data);
+				console.log("Got me sum data:", data);
 
-			// Loop over all users
-			data.forEach(user => {
-				// Append a listitem for each user
-				document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
-			});
+				// Loop over all users
+				data.forEach(user => {
+					// Append a listitem for each user
+					document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
+				});
 
-		} else {
-			// Something went wrong :'(
-			console.log("ERROR ERROR DANGER WILL ROBINSON!");
+			} else {
+				// Something went wrong :'(
+				console.log("ERROR ERROR DANGER WILL ROBINSON!");
+			}
 		}
-	}
-});
+	});
 
-// Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
-request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+	// Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
+	request.open('GET', 'https://jsonplaceholder.typicode.com/users');
 
-// Send the request
-request.send();
+	// Send the request
+	request.send();
+
+	console.log("Request sent!");
+}
+
+// Get users
+getUsers();
 
 // Done?
-console.log("Request sent!");
+console.log("All requests sent!");
