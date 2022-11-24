a8276975ff9adb5d302927de7f67f90562335f41
Johan Nordström <johan@digitalvillage.se>
2022-11-24 10:20:09 +0100

    Ooops, previous commit had the wrong URL.

--- 29-xmlhttprequest/script.js
+++ 29-xmlhttprequest/script.js
@@ -30,7 +30,7 @@ const getUsers = (callback) => {
 	});
 
 	// Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
-	request.open('GET', 'https://jsonplaceholder.typicode.com/lolcats');
+	request.open('GET', 'https://jsonplaceholder.typicode.com/users');
 
 	// Send the request
 	request.send();
