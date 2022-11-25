dad542b1ee9bc64ad6743c0d433422d792627918
Johan Nordström <johan@digitalvillage.se>
2022-11-25 11:39:50 +0100

    Fetch boilerplate.

---
+++ 33-async-await/script.js
@@ -0,0 +1,21 @@
+/**
+ * async/await
+ *
+ */
+
+/*
+fetch('data/cats.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+
+		return response.json();
+	})
+	.then(data => {
+		console.log("Gots data:", data);
+	})
+	.catch(err => {
+		console.log("Something blew up 😢:", err);
+	});
+*/
