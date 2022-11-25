90216a5f23a38e041ab5da4a908fea5f271aa2f3
Johan Nordström <johan@digitalvillage.se>
2022-11-25 11:22:56 +0100

    Fetch API 🤯.

--- 32-fetch/script.js
+++ 32-fetch/script.js
@@ -3,3 +3,33 @@
  *
  */
 
+fetch('data/cats.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+
+		return response.json();
+	})
+	.then(data => {
+		console.log("Gots data:", data);
+	})
+	.catch(err => {
+		console.log("Something blew up 😢:", err);
+	});
+
+
+fetch('data/dogs.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+
+		return response.json();
+	})
+	.then(data => {
+		console.log("Gots data:", data);
+	})
+	.catch(err => {
+		console.log("Something blew up 😢:", err);
+	});
