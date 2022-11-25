6914826ab86dd5f86f7edbd5d3be0df61b78c453
Johan Nordström <johan@digitalvillage.se>
2022-11-25 13:33:29 +0100

    Chuck Norris Joke of the Day - Promise version.

--- 34-chuck-norris/script.js
+++ 34-chuck-norris/script.js
@@ -5,3 +5,20 @@
  * <https://api.chucknorris.io/jokes/random>
  */
 
+fetch('https://api.chucknorris.io/jokes/random')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+
+		return response.json();
+	})
+	.then(joke => {
+		document.querySelector('#joke').innerText = joke.value;
+
+	})
+	.catch(err => {
+		console.log("ERROR: Could not fetch joke. Reason:", err);
+
+		document.querySelector('#joke').innerText = "ERROR: You don't summon Chuck Norris, Chuck Norris summons you";
+	});
