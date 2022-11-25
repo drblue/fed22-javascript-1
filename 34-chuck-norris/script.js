38ea84405c875330adc71acdf916b54a820adbad
Johan Nordström <johan@digitalvillage.se>
2022-11-25 13:41:13 +0100

    Chuck Norris Joke of the Day - But with button for moar lolz!

--- 34-chuck-norris/script.js
+++ 34-chuck-norris/script.js
@@ -5,20 +5,29 @@
  * <https://api.chucknorris.io/jokes/random>
  */
 
-fetch('https://api.chucknorris.io/jokes/random')
-	.then(response => {
-		if (!response.ok) {
-			throw new Error("Response was not OK!");
-		}
+const getJoke = () => {
+	fetch('https://api.chucknorris.io/jokes/random')
+		.then(response => {
+			if (!response.ok) {
+				throw new Error("Response was not OK!");
+			}
 
-		return response.json();
-	})
-	.then(joke => {
-		document.querySelector('#joke').innerText = joke.value;
+			return response.json();
+		})
+		.then(joke => {
+			document.querySelector('#joke').innerText = joke.value;
 
-	})
-	.catch(err => {
-		console.log("ERROR: Could not fetch joke. Reason:", err);
+		})
+		.catch(err => {
+			console.log("ERROR: Could not fetch joke. Reason:", err);
 
-		document.querySelector('#joke').innerText = "ERROR: You don't summon Chuck Norris, Chuck Norris summons you";
-	});
+			document.querySelector('#joke').innerText = "ERROR: You don't summon Chuck Norris, Chuck Norris summons you";
+		});
+}
+
+document.querySelector('#getJokeBtn').addEventListener('click', () => {
+	getJoke();
+});
+
+// Start of with a joke
+getJoke();
