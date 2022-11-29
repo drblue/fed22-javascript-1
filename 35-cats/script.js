4a003a51bfa9cdc6c9cb3355375d3e258ff2a4d9
Johan Nordström <johan@digitalvillage.se>
2022-11-29 10:01:36 +0100

    Cats as a Service workshop solution.

--- 35-cats/script.js
+++ 35-cats/script.js
@@ -5,3 +5,25 @@
  * <https://cataas.com/cat?json=true>
  */
 
+const baseUrl = 'https://cataas.com';
+
+const get = async (url) => {
+	const response = await fetch(url);
+	if (!response.ok) {
+		throw new Error(`Response was not OK. Status returned was "${res.status} ${res.statusText}".`);
+	}
+	return await response.json();
+}
+
+const getNewCat = async () => {
+	const cat = await get(`${baseUrl}/cat?json=true`)
+	document.querySelector('#catImg').src = baseUrl + cat.url;
+}
+
+// get a mjau-mjau when button is pressed
+document.querySelector('#getCatBtn').addEventListener('click', () => {
+	getNewCat();
+});
+
+// start of with a cat
+getNewCat();
