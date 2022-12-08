3a4fa26f41f59860d21b8ca38209f4153e72c5e5
Johan Nordström <johan@digitalvillage.se>
2022-12-08 10:11:22 +0100

    Installed Bootstrap, replaced HTML and CSS with content from `27-todos`.

--- 39-typescript-vite/src/main.ts
+++ 39-typescript-vite/src/main.ts
@@ -1,13 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
 const todosList = document.querySelector('#todos')
 const newTodoForm = document.querySelector('#new-todo-form')
-
-const getRandomNumber = (max: number = 10) => {
-	return Math.ceil( Math.random() * max )
-}
-const num = getRandomNumber(40)
-console.log("Random number:", num)
 
 newTodoForm?.addEventListener('submit', e => {
 	e.preventDefault()
