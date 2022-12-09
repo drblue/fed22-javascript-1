f06470843c2ae77472a39779bd8a47ebb2bc6937
Johan Nordström <johan@digitalvillage.se>
2022-12-09 11:38:03 +0100

    Fetch todos from server.

--- 40-async-todos-ts/frontend/src/main.ts
+++ 40-async-todos-ts/frontend/src/main.ts
@@ -1,2 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
+
+interface ITodo {
+	id: number,
+	title: string,
+	completed: boolean,
+}
+
+/**
+ * Fetch todos from server
+ */
+const fetchTodos = async () => {
+	const res = await fetch('http://localhost:3001/todos')
+	if (!res.ok) {
+		throw new Error(`${res.status} ${res.statusText}`)
+	}
+
+	return await res.json() as ITodo[]
+}
+
+/**
+ * Get todos from server, update `todos`-array and render todos.
+ */
+const getTodos = async () => {
+	// Fetch todos from server
+	const omgdata = await fetchTodos()
+	console.log(omgdata)
+
+	// Update `todos`-array
+
+	// Render todos
+}
+
+getTodos()
